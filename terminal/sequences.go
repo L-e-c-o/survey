fa9fa37325384393404dc0ec832ea874df4646c7
you@example.com <you@example.com>
2023-02-12 20:50:02 +0100

    trying

--- terminal/sequences.go
+++ terminal/sequences.go
@@ -24,7 +24,7 @@ const (
 	SpecialKeyDelete   = '\x12'
 	IgnoreKey          = '\000'
 	KeyTab             = '\t'
-	KeySave            = '\x00'
+	KeySave            = '\x10'
 )
 
 func soundBell(out io.Writer) error {
